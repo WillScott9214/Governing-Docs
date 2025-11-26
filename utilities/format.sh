b68ce9444628814e1f8acca934fbb192c4f89db5
EmAtt04 <85852330+EmAtt04@users.noreply.github.com>
2025-11-25 21:29:20 -0500

    updated pdf and format (#93)

--- utilities/format.sh
+++ utilities/format.sh
@@ -1,4 +1,4 @@
-latexindent -s -w -l format.yaml -c=build_files/ -m bylaws.tex Sections/*.tex Sections/Exec\ Portfolios/*.tex
+latexindent -s -w -l utilities/format.yaml -c=build_files/ -m bylaws.tex Sections/*.tex Sections/Exec\ Portfolios/*.tex
 
 if [ $# -gt 0 ]; then
   git restore bylaws.pdf
